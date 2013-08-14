46db619601baeeec7d59b874359cab97567ac12f
Jong Han <jh896@cl.cam.ac.uk>
2013-08-14 17:03:38 +0100

    Added pcie programming and simulation tasks.

--- projects/reference_nic/hw/make_pkts.py
+++ projects/reference_nic/hw/make_pkts.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python-nf
+#!/usr/bin/env python
 
 ################################################################################
 #
@@ -15,6 +15,7 @@
 #
 #  Author:
 #        James Hongyi Zeng, Gianni Antichi
+#        Jong Hun Han
 #
 #  Description:
 #        An example of how to use scapy to build packets.  The packet in
@@ -54,6 +55,7 @@ from __future__ import with_statement
 
 import os
 import sys
+import argparse
 
 script_dir = os.path.dirname( sys.argv[0] )
 # Add path *relative to this script's location* of axitools module
@@ -64,14 +66,34 @@ import axitools
 
 from scapy.layers.all import Ether, IP, TCP
 
+# Packet length parameter
+parser = argparse.ArgumentParser()
+parser.add_argument("--packet_length", help="Length of packet in no of bytes")
+parser.add_argument("--packet_no", help="No of packets")
+
+args = parser.parse_args()
+
+if (args.packet_length):
+	pkt_length = args.packet_length
+else:
+	pkt_length = '16'
+
+payload_data = ''
+
+if (args.packet_no):
+	no_pkts = args.packet_no
+else:
+	no_pkts = '2'
+
+for i in range(int(pkt_length)):
+	payload_data = payload_data + 'A'
 
 pkts=[]
 # A simple TCP/IP packet embedded in an Ethernet II frame
-for i in range(8):
+for i in range(int(no_pkts)*4):
     pkt = (Ether(src='11:22:33:44:55:66', dst='77:88:99:aa:bb:cc')/
            IP(src='192.168.1.1', dst='192.168.1.2')/
-           TCP()/
-           'Hello, NetFPGA-10G!')
+           TCP()/payload_data)
     pkt.time        = i*(1e-8)
     # Set source network interface for DMA stream
     pkt.tuser_sport = 1 << (i%4*2 + 1) # PCI ports are odd-numbered
@@ -82,12 +104,19 @@ with open( os.path.join( script_dir, 'dma_0_stim.axi' ), 'w' ) as f:
     axitools.axis_dump( pkts, f, 256, 1e-9 )
 with open( os.path.join( script_dir, 'dma_0_expected.axi' ), 'w' ) as f:
     axitools.axis_dump( pkts*4, f, 256, 1e-9 )
+
+nf_pkts=[]
+if (args.packet_no):
+	nf_pkts = pkts[0:int(no_pkts)]
+else:
+	nf_pkts = pkts
+
 # 10g interfaces
 for i in range(4):
     # replace source port
-    for pkt in pkts:
+    for pkt in nf_pkts:
         pkt.tuser_sport = 1 << (i*2) # physical ports are even-numbered
     with open( os.path.join( script_dir, 'nf10_10g_interface_%d_stim.axi' % i ), 'w' ) as f:
-        axitools.axis_dump( pkts, f, 256, 1e-9 )
+        axitools.axis_dump( nf_pkts, f, 256, 1e-9 )
     with open( os.path.join( script_dir, 'nf10_10g_interface_%d_expected.axi' % i ), 'w' ) as f:
         axitools.axis_dump( pkts[0:2], f, 256, 1e-9 )
