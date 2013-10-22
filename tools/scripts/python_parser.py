f94fabdddd650f4b3753f5b229e420a0889ba72f
manihatn <manihatn@gmail.com>
2013-10-22 13:56:47 +0100

    faster and sleeker python_parser.py

--- tools/scripts/python_parser.py
+++ tools/scripts/python_parser.py
@@ -1,39 +1,62 @@
 #!/usr/bin/python
 
-# Description:
-# This python parser takes the xpararamters.h file generated
-# by doing a "make xparam" at the hw folder inside the project
-# of your interest. xparameter.h file should be given as input 
-# to this file by placing it in this folder. Then when you run
-# this script, it will generate the reg_defines.py file which 
-# can be used by the HW tests to access the registers by placing
-# them inside the project specific python lib 
-# (ex: xyz_proj/lib/Python/). Please remember to suffix the name
-# of the project after reg_defines.py (ex: reg_defines_xyz_proj.py)
-# only then the test scripts can recognize this file.
-
-
+################################################################################
+#
+#  NetFPGA-10G http://www.netfpga.org
+#
+#  File:
+#        xparam2regdefines.py
+#
+#  Project:
+#        
+#
+#  Module:
+#        
+#
+#  Author:
+#        Neelakandan Manihatty Bojan
+#
+#  Description:
+#        This is used to convert xparameters.h to reg_defines.h
+#
+#  Copyright notice:
+#        Copyright (C) 2013 University of Cambridge
+#
+#  Licence:
+#        This file is part of the NetFPGA 10G development base package.
+#
+#        This file is free code: you can redistribute it and/or modify it under
+#        the terms of the GNU Lesser General Public License version 2.1 as
+#        published by the Free Software Foundation.
+#
+#        This package is distributed in the hope that it will be useful, but
+#        WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#        Lesser General Public License for more details.
+#
+#        You should have received a copy of the GNU Lesser General Public
+#        License along with the NetFPGA source package.  If not, see
+#        http://www.gnu.org/licenses/.
+#
+#
 import re
 
-inputfile=open('xparameters.h',"r")
-str1=inputfile.read()
-inputfile.close()
+input_file = open("reg_defines.h", "r")
+output_file = open("reg_defines.py", "w")
+output_file.write("#!/usr/bin/python")
+for line in input_file:
+    match_defines = re.match(r'\s*#define ([a-zA-Z_0-9]+) (.*)', line)
+    match_comments = re.match(r'\s*[/\*][\s*\*](.*)', line)
+ 	
+    if match_defines:
+        newline1= "\ndef %s:\n    return %s" % (match_defines.group(1),match_defines.group(2))
+	output_file.write(newline1)
 
-for line in str1:
-	match = re.compile(r'\s*#define ([a-zA-Z_0-9]+) (.*)')
-	str2=match.sub(r'\ndef \1():\n    return \2\n', str1)
-outputfile=open("temp.py","w")
-outputfile.write(str2)
-outputfile.close()
+    elif match_comments:
+	newline2= "\n# %s" % (match_comments.group(1))
+        output_file.write(newline2)
 
-regdefinesfile=open('temp.py',"r")
-str3=regdefinesfile.read()
-regdefinesfile.close()
+    else:
+        output_file.write(line)
 
-for line in str3:
-	match =	re.compile(r'\s*[/\*][\s*\*](.*)')
-	str4=match.sub(r'\n# \1\n',str3)
-outputfile1=open("reg_defines.py","w")
-outputfile1.write("#!/usr/bin/python")
-outputfile1.write(str4)
-outputfile1.close()
+
