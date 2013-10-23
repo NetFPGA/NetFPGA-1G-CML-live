603d5b0ee226aff404a22582340f124a4320f3f7
manihatn <manihatn@gmail.com>
2013-10-23 17:44:03 +0100

    updated register values for reference_switch

--- projects/reference_switch/sw/host/register_monitor/switch_regs.c
+++ projects/reference_switch/sw/host/register_monitor/switch_regs.c
@@ -13,7 +13,7 @@ int main(){
     printf("---------------------------------------------------------\n");
     printf("|             Switch pcore registers                    |\n");
     printf("---------------------------------------------------------\n");
-    printf("Type                                |offset |\tValue   |\n");
+    printf("Type\t\t\t\t|offset |\tValue   |\n");
     printf("---------------------------------------------------------\n");
     printf("RESET_CNTRS\t\t\t|00\t|       %llx \t|\n", regread(XPAR_NF10_SWITCH_OUTPUT_PORT_LOOKUP_0_RESET_CNTRS_OFFSET));
     printf("SWITCH_OP_LUT_NUM_HITS_REG\t|04\t|       %llx \t|\n", regread(XPAR_NF10_SWITCH_OUTPUT_PORT_LOOKUP_0_LUT_NUM_HITS_REG_OFFSET));
