09589397c55f283d2523460e83edb14cd1cd8508
manihatn <nm525@cl.cam.ac.uk>
2013-12-13 15:26:55 +0000

    updated on how reg_defines is called

--- lib/python/NFTest/NFTestHeader.py
+++ lib/python/NFTest/NFTestHeader.py
@@ -7,7 +7,7 @@ sys.path.append(os.path.abspath(os.environ['NF_DESIGN_DIR']+'/lib/Python'))
 project = os.path.basename(os.environ['NF_DESIGN_DIR'])
 if project == '':
     project = os.path.basename(os.environ['NF_DESIGN_DIR'][0:-1])
-reg_defines = __import__('reg_defines_'+project)
+#reg_defines = __import__('reg_defines_'+project)
 
 # scapy
 try:
