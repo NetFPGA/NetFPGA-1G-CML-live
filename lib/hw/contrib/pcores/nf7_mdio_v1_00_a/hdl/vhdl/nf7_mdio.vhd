7075f96a56f52038338896ad906e28f3ccf8e9b0
Jack Meador <jack.meador@cmlab.biz>
2014-01-09 16:10:14 -0700

    update headers for nf7_mdio

--- lib/hw/contrib/pcores/nf7_mdio_v1_00_a/hdl/vhdl/nf7_mdio.vhd
+++ lib/hw/contrib/pcores/nf7_mdio_v1_00_a/hdl/vhdl/nf7_mdio.vhd
@@ -1,3 +1,41 @@
+--
+--  NetFPGA-1G-CML http://www.netfpga.org
+-- 
+-- File:
+--              nf7_mdio.vhd
+--
+-- Library:
+--              hw/contrib/pcores/nf7_mdio_v1_00_a
+--
+-- Module:
+--              nf7_mdio_core
+--
+-- Author:
+--              Jack Meador
+-- 
+-- Description:
+--              MDIO interface wrapper customized for use with NetFPGA-1G-CML
+--
+-- Copyright notice:
+--              Copyright (C) 2013 Computer Measurement Laboratory, LLC
+--
+-- License:
+--        This file is part of the NetFPGA 10G development base package.
+--
+--        This file is free code: you can redistribute it and/or modify it under
+--        the terms of the GNU Lesser General Public License version 2.1 as
+--        published by the Free Software Foundation.
+--
+--        This package is distributed in the hope that it will be useful, but
+--        WITHOUT ANY WARRANTY; without even the implied warranty of
+--        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+--        Lesser General Public License for more details.
+--
+--        You should have received a copy of the GNU Lesser General Public
+--        License along with the NetFPGA source package.  If not, see
+--        http://www.gnu.org/licenses/.
+--
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
