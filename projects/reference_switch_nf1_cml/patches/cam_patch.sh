cd92b9512e5ca7ed96d3eee646eaf69e3810f449
David Van Arnem <dvanarnem@cmlab.biz>
2014-03-06 16:39:10 -0700

    Updated the patch files for the CAM generator to make a working design.

--- projects/reference_switch_nf1_cml/patches/cam_patch.sh
+++ projects/reference_switch_nf1_cml/patches/cam_patch.sh
@@ -3,4 +3,7 @@
 patch ../implement/CustomizeWrapper.pl CustomizeWrapper.pl.patch
 patch ../implement/vhdl_xst.scr vhdl_xst.scr.patch
 patch ../src/vhdl/cam_pkg.vhd cam_pkg.vhd.patch
+patch ../src/vhdl/cam_control.vhd cam_control.vhd.patch
+patch ../src/vhdl/cam_mem_blk.vhd cam_mem_blk.vhd.patch
+patch ../src/vhdl/cam_mem_blk_extdepth_prim.vhd cam_mem_blk_extdepth_prim.vhd.patch
 patch ../src/vhdl/cam_rtl.vhd cam_rtl.vhd.patch
