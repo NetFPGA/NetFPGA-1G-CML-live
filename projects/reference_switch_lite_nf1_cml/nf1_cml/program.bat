5bac474fe10c12cd40def8787471aa4c41f35883
David Van Arnem <dvanarnem@cmlab.biz>
2014-05-05 10:59:12 -0600

    Added "download" rule to nf1_cml project Makefiles.
    Closes cmlab/NetFPGA-1G-CML#3.

---
+++ projects/reference_switch_lite_nf1_cml/nf1_cml/program.bat
@@ -0,0 +1,6 @@
+setMode -bscan
+setCable -p auto
+identify
+assignfile -p 1 -file bitfiles/download.bit
+program -p 1
+quit
---
+++ projects/reference_switch_lite_nf1_cml/nf1_cml/xmd-run.tcl
@@ -0,0 +1,6 @@
+connect mb mdm 
+stop
+rst
+dow sw/embedded/result/loopback_test_nf1_cml.elf
+run
+exit
