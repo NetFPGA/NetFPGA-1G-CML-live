49b486fad41e36ad311a4f9efc877ef5f251766d
Jay Hirata <jhirata@cmlab.biz>
2014-12-10 16:20:34 -0700

    Add nf1_cml_io_example contrib project
    This project provides a reference for the onboard buttons and LEDs as
    well as the SD card, Pmods, and DDR3.

---
+++ contrib-projects/nf1_cml_io_example/hw/data/system.xdc
@@ -0,0 +1,7 @@
+#
+# pin constraints
+#
+#
+# additional constraints
+#
+create_clock -name sys_clk_pin -period "5.0" [get_ports "CLK_P"]
---
+++ contrib-projects/nf1_cml_io_example/tools/download.tcl
@@ -0,0 +1,37 @@
+################################################################################
+#
+#  NetFPGA-1G-CML http://www.netfpga.org
+#
+#  Project:
+#        nf1_cml_io_example
+#
+#  Author:
+#        Jay Hirata
+#
+#  Copyright notice:
+#        Copyright (C) 2014 Computer Measurement Laboratory
+#
+#  Licence:
+#        This file is part of the NetFPGA-1G-CML development base package.
+#
+#        This file is free code: you can redistribute it and/or modify it under
+#        the terms of the GNU Lesser General Public License version 2.1 as
+#        published by the Free Software Foundation.
+#
+#        This package is distributed in the hope that it will be useful, but
+#        WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#        Lesser General Public License for more details.
+#
+#        You should have received a copy of the GNU Lesser General Public
+#        License along with the NetFPGA source package.  If not, see
+#        http://www.gnu.org/licenses/.
+#
+################################################################################
+
+connect mb mdm
+rst
+stop
+dow ./sw/embedded/executable.elf
+run
+exit
